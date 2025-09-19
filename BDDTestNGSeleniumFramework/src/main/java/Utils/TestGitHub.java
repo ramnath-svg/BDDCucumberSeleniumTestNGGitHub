592a8320bb758b8f3234127e29b36cf9f1650034
Raj Nayak <emailtorajashree@gmail.com>
2025-09-19 10:22:28 +0530

    Commited to new repo

---
+++ BDDTestNGSeleniumFramework/src/main/java/Utils/TestGitHub.java
@@ -0,0 +1,9 @@
+package Utils;
+
+public class TestGitHub {
+	public TestGitHub() {
+		System.out.println("Test 1234");
+		System.out.println("Test push23423");
+		System.out.println("Krishna Udupi");
+	}
+}

5a68adaf0317e7fa2ec0fe8bcccd02891b9f9809
Raj Nayak <emailtorajashree@gmail.com>
2025-09-20 18:07:16 +0530

    Added TestPull Request Git Hub

--- BDDTestNGSeleniumFramework/src/main/java/Utils/featureLoginBranchTest.java
+++ BDDTestNGSeleniumFramework/src/main/java/Utils/featureLoginBranchTest.java
@@ -3,6 +3,6 @@ package Utils;
 public class featureLoginBranchTest {
 	public void featurelogin() {
 		
-		System.out.println("Krishna Please help me god-pushed changes only to main");
+		System.out.println("Krishna Please help me god-pushed changes only to main 12312");
 		}
 }
